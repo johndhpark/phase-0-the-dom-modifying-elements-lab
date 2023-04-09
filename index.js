60add6863ddfd39ab2d514beb718720eb427783f
John DH. Park <john.dh.park@me.com>
2023-04-09 08:11:40 -0700

    completed dom-modifying-elements-lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+const mainElement = document.querySelector("#main");
+mainElement.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Jon is the champion";
